f9b55f1141cb846048baa0d03782fc590515ab5f
Aarshdeep Singh Chadha <ascnyc29@gmail.com>
2024-02-11 17:37:45 +0530

    implemented TinyMCE for textAREA tool

--- BlukyWebMVC/Areas/Admin/Controllers/ProductController.cs
+++ BlukyWebMVC/Areas/Admin/Controllers/ProductController.cs
@@ -21,7 +21,7 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
             return View(objProductList);
         }
 
-        public IActionResult Create()
+        public IActionResult Upsert(int? id)
         {
             IEnumerable<SelectListItem> CategoryList = _unitOfWork.Category.GetAll().Select(x => new SelectListItem
             {
@@ -38,10 +38,21 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
                 CategoryList = CategoryList
             };
 
-            return View(productVM);
+            if(id == null || id ==0)
+            {
+                //Create
+                return View(productVM);
+
+            }
+            else
+            {
+                //Update
+                productVM.Product = _unitOfWork.Product.Get(x => x.Id == id);
+                return View(productVM);
+            }
         }
         [HttpPost]
-        public IActionResult Create(ProductVM productVM)
+        public IActionResult Upsert(ProductVM productVM, IFormFile? file)
         {
             if (ModelState.IsValid)
             {
@@ -61,34 +72,6 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
             }
 
         }
-
-        public IActionResult Edit(int? id)
-        {
-            if (id == null || id == 0)
-            {
-                return NotFound();
-            }
-            Product? productFromDb = _unitOfWork.Product.Get(c => c.Id == id);
-            if (productFromDb == null)
-            {
-                return NotFound();
-            }
-            return View(productFromDb);
-        }
-        [HttpPost]
-        public IActionResult Edit(Product obj)
-        {
-            if (ModelState.IsValid)
-            {
-                _unitOfWork.Product.Update(obj);
-                _unitOfWork.Save();
-                TempData["success"] = "Product updated successfully.";
-                return RedirectToAction("Index");
-            }
-            return View();
-
-        }
-
         public IActionResult Delete(int? id)
         {
             if (id == null || id == 0)
