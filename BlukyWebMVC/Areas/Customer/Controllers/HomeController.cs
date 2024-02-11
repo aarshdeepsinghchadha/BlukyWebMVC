1656fe9fdf45c9b557eddd5737e005c65e2d6272
Aarshdeep Singh Chadha <ascnyc29@gmail.com>
2024-02-11 20:23:41 +0530

    added home [page customer facing and details page

--- BlukyWebMVC/Areas/Customer/Controllers/HomeController.cs
+++ BlukyWebMVC/Areas/Customer/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
+using BulkyBook.DataAccess.Repository.IRepository;
 using BulkyBook.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Security.Cryptography.Xml;
 
 namespace BulkyBookWeb.Areas.Customer.Controllers
 {
@@ -8,15 +10,24 @@ namespace BulkyBookWeb.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includePRoperties: "Category");
+            return View(productList);
+        }
+
+        public IActionResult Details(int productId)
+        {
+            Product product = _unitOfWork.Product.Get(x=>x.Id == productId, includePRoperties: "Category");
+            return View(product);
         }
 
         public IActionResult Privacy()
