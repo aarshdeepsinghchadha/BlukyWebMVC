7614a590ab8a4d6114f891464c5ee51ce2c3e78b
Aarshdeep Singh Chadha <ascnyc29@gmail.com>
2024-02-11 13:56:39 +0530

    completed the Product Controller with its views and action method

---
+++ Bulky.Models/Product.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBook.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string Description { get; set; }
+        [Required]
+        public string ISBN { get; set; }
+        [Required]
+        public string Author { get; set; }
+        [Required]
+        [Display(Name = "ListPrice")]
+        [Range(1, 1000)]
+        public double ListPrice { get; set; }
+        [Required]
+        [Display(Name = "Price fro 1-50")]
+        [Range(1, 1000)]
+        public double Price { get; set; }
+        [Required]
+        [Display(Name = "Price fro 50+")]
+        [Range(1, 1000)]
+        public double Price50 { get; set; }
+        [Required]
+        [Display(Name = "Price fro 100+")]
+        [Range(1, 1000)]
+        public double Price100 { get; set; }
+
+    }
+}
