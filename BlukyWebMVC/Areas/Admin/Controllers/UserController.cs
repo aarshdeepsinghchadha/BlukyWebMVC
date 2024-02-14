fa786d3a705343a8d5b515a8f7c8114192dea877
Aarshdeep Singh Chadha <ascnyc29@gmail.com>
2024-02-14 12:35:01 +0530

    completed

---
+++ BlukyWebMVC/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,130 @@
+﻿using BulkyBook.DataAccess.Data;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using BulkyBook.Models.ViewModels;
+using BulkyBook.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace BulkyBookWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.Role_Admin)]
+    public class UserController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly ApplicationDbContext _db;
+        private readonly IUnitOfWork _unitOfWork;
+        public UserController(UserManager<IdentityUser> userManager, IUnitOfWork unitOfWork, RoleManager<IdentityRole> roleManager, ApplicationDbContext db)
+        {
+            _unitOfWork = unitOfWork;
+            _roleManager = roleManager;
+            _userManager = userManager;
+            _db = db;
+        }
+        public IActionResult Index() 
+        {
+            return View();
+        }
+
+        public IActionResult RoleManagment(string userId) {
+
+            string RoleID = _db.UserRoles.FirstOrDefault(x=>x.UserId == userId).RoleId;
+            RoleManagmentVM RoleVM = new RoleManagmentVM() {
+                ApplicationUser = _unitOfWork.ApplicationUser.Get(u => u.Id == userId, includeProperties:"Company"),
+                RoleList = _roleManager.Roles.Select(i => new SelectListItem {
+                    Text = i.Name,
+                    Value = i.Name
+                }),
+                CompanyList = _unitOfWork.Company.GetAll().Select(i => new SelectListItem {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                }),
+            };
+
+            RoleVM.ApplicationUser.Role = _db.Roles.FirstOrDefault(x => x.Id == RoleID).Name;
+            return View(RoleVM);
+        }
+
+        [HttpPost]
+        public IActionResult RoleManagment(RoleManagmentVM roleManagmentVM) {
+
+            string RoelID  = _db.UserRoles.FirstOrDefault(x => x.UserId == roleManagmentVM.ApplicationUser.Id).RoleId;
+            string oldRole = _db.Roles.FirstOrDefault(x => x.Id == RoelID).Name;
+
+            ApplicationUser applicationUser = _db.ApplicationUsers.FirstOrDefault(x => x.Id == roleManagmentVM.ApplicationUser.Id);
+
+            if (!(roleManagmentVM.ApplicationUser.Role == oldRole)) {
+                //a role was updated
+
+                if(roleManagmentVM.ApplicationUser.Role == SD.Role_Company)
+                {
+                    applicationUser.CompanyId = roleManagmentVM.ApplicationUser.CompanyId;
+                }
+                if(oldRole == SD.Role_Company)
+                {
+                    applicationUser.CompanyId = null;
+                }
+                _db.SaveChanges();
+
+                _userManager.RemoveFromRoleAsync(applicationUser,oldRole).GetAwaiter().GetResult();
+                _userManager.AddToRoleAsync(applicationUser, roleManagmentVM.ApplicationUser.Role).GetAwaiter().GetResult();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<ApplicationUser> objUserList = _db.ApplicationUsers.Include(x => x.Company).ToList();
+            var userRoles = _db.UserRoles.ToList();
+            var roles = _db.Roles.ToList();
+            foreach(var user in objUserList) {
+
+                var roleId = userRoles.FirstOrDefault(x => x.UserId == user.Id).RoleId;
+                user.Role = roles.FirstOrDefault(x => x.Id == roleId).Name;
+
+                if (user.Company == null) {
+                    user.Company = new Company() {
+                        Name = ""
+                    };
+                }
+            }
+
+            return Json(new { data = objUserList });
+        }
+
+
+        [HttpPost]
+        public IActionResult LockUnlock([FromBody]string id)
+        {
+
+            var objFromDb = _db.ApplicationUsers.FirstOrDefault(x=>x.Id == id);
+            if (objFromDb == null) 
+            {
+                return Json(new { success = false, message = "Error while Locking/Unlocking" });
+            }
+
+            if(objFromDb.LockoutEnd!=null && objFromDb.LockoutEnd > DateTime.Now) {
+                //user is currently locked and we need to unlock them
+                objFromDb.LockoutEnd = DateTime.Now;
+            }
+            else {
+                objFromDb.LockoutEnd = DateTime.Now.AddYears(1000);
+            }
+            _db.SaveChanges();
+            return Json(new { success = true, message = "Operation Successful" });
+        }
+
+        #endregion
+    }
+}
--- Bulky.DataAccess/DbInitializer/DbInitializer.cs
+++ Bulky.DataAccess/DbInitializer/DbInitializer.cs
@@ -4,10 +4,7 @@ using BulkyBook.Utility;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BulkyBook.DataAccess.DbInitializer
 {
@@ -30,28 +27,46 @@ namespace BulkyBook.DataAccess.DbInitializer
 
         public void Initialize()
         {
-            //migrations if they are not applied
+            // Migrate database if pending migrations exist
             try
             {
-                if (_db.Database.GetPendingMigrations().Count() > 0)
+                if (_db.Database.GetPendingMigrations().Any())
                 {
                     _db.Database.Migrate();
                 }
             }
-            catch (Exception ex) { }
-
-
-
-            //create roles if they are not created
-            if (!_roleManager.RoleExistsAsync(SD.Role_Customer).GetAwaiter().GetResult())
+            catch (Exception ex)
             {
-                _roleManager.CreateAsync(new IdentityRole(SD.Role_Customer)).GetAwaiter().GetResult();
-                _roleManager.CreateAsync(new IdentityRole(SD.Role_Employee)).GetAwaiter().GetResult();
-                _roleManager.CreateAsync(new IdentityRole(SD.Role_Admin)).GetAwaiter().GetResult();
-                _roleManager.CreateAsync(new IdentityRole(SD.Role_Company)).GetAwaiter().GetResult();
+                // Handle migration exception
+            }
 
-                //if roles are not created, then we will create admin user as well
-                _userManager.CreateAsync(new ApplicationUser
+            SeedRoles();
+            SeedAdminUser();
+            SeedCategories();
+            SeedCompanies();
+            SeedProducts();
+
+            return;
+        }
+
+        private void SeedRoles()
+        {
+            string[] roles = { SD.Role_Customer, SD.Role_Employee, SD.Role_Admin, SD.Role_Company };
+
+            foreach (var role in roles)
+            {
+                if (!_roleManager.RoleExistsAsync(role).GetAwaiter().GetResult())
+                {
+                    _roleManager.CreateAsync(new IdentityRole(role)).GetAwaiter().GetResult();
+                }
+            }
+        }
+
+        private void SeedAdminUser()
+        {
+            if (!_userManager.Users.Any(u => u.Email == "ascnyc29@gmail.com"))
+            {
+                var adminUser = new ApplicationUser
                 {
                     UserName = "ascnyc29@gmail.com",
                     Email = "ascnyc29@gmail.com",
@@ -61,17 +76,137 @@ namespace BulkyBook.DataAccess.DbInitializer
                     State = "IL",
                     PostalCode = "23422",
                     City = "Chicago"
-                }, "Pa$$w0rd*").GetAwaiter().GetResult();
+                };
 
-
-                ApplicationUser user = _db.ApplicationUsers.FirstOrDefault(u => u.Email == "ascnyc29@gmail.com");
-                _userManager.AddToRoleAsync(user, SD.Role_Admin).GetAwaiter().GetResult();
+                var result = _userManager.CreateAsync(adminUser, "Pa$$w0rd*").GetAwaiter().GetResult();
+                if (result.Succeeded)
+                {
+                    _userManager.AddToRoleAsync(adminUser, SD.Role_Admin).GetAwaiter().GetResult();
+                }
             }
-
-
-            return;
-
         }
 
+        private void SeedCategories()
+        {
+            if (!_db.Categories.Any())
+            {
+                _db.Categories.AddRange(
+                    new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                    new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
+                    new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+                );
+
+                _db.SaveChanges();
+            }
+        }
+
+        private void SeedCompanies()
+        {
+            if (!_db.Companies.Any())
+            {
+                _db.Companies.AddRange(
+                    new Company { Id = 1, Name = "Tech Solution", StreetAddress = "123 Tech St", City = "Tech City", PostalCode = "12121", State = "IL", PhoneNumber = "6669990000" },
+                    new Company { Id = 2, Name = "Vivid Books", StreetAddress = "999 Vid St", City = "Vid City", PostalCode = "66666", State = "IL", PhoneNumber = "7779990000" },
+                    new Company { Id = 3, Name = "Readers Club", StreetAddress = "999 Main St", City = "Lala land", PostalCode = "99999", State = "NY", PhoneNumber = "1113335555" }
+                );
+
+                _db.SaveChanges();
+            }
+        }
+
+        private void SeedProducts()
+        {
+            if (!_db.Products.Any())
+            {
+                _db.Products.AddRange(
+                    new Product
+                    {
+                        Id = 1,
+                        Title = "Fortune of Time",
+                        Author = "Billy Spark",
+                        Description = "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ",
+                        ISBN = "SWD9999001",
+                        ListPrice = 99,
+                        Price = 90,
+                        Price50 = 85,
+                        Price100 = 80,
+                        CategoryId = 1,
+                        ImageUrl = ""
+                    },
+                    new Product
+                    {
+                        Id = 2,
+                        Title = "Dark Skies",
+                        Author = "Nancy Hoover",
+                        Description = "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ",
+                        ISBN = "CAW777777701",
+                        ListPrice = 40,
+                        Price = 30,
+                        Price50 = 25,
+                        Price100 = 20,
+                        CategoryId = 2,
+                        ImageUrl = ""
+                    },
+                new Product
+                {
+                    Id = 3,
+                    Title = "Vanish in the Sunset",
+                    Author = "Julian Button",
+                    Description = "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ",
+                    ISBN = "RITO5555501",
+                    ListPrice = 55,
+                    Price = 50,
+                    Price50 = 40,
+                    Price100 = 35,
+                    CategoryId = 3,
+                    ImageUrl = ""
+                },
+                new Product
+                {
+                    Id = 4,
+                    Title = "Cotton Candy",
+                    Author = "Abby Muscles",
+                    Description = "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ",
+                    ISBN = "WS3333333301",
+                    ListPrice = 70,
+                    Price = 65,
+                    Price50 = 60,
+                    Price100 = 55,
+                    CategoryId = 1,
+                    ImageUrl = ""
+                },
+                new Product
+                {
+                    Id = 5,
+                    Title = "Rock in the Ocean",
+                    Author = "Ron Parker",
+                    Description = "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ",
+                    ISBN = "SOTJ1111111101",
+                    ListPrice = 30,
+                    Price = 27,
+                    Price50 = 25,
+                    Price100 = 20,
+                    CategoryId = 1,
+                    ImageUrl = ""
+                },
+                new Product
+                {
+                    Id = 6,
+                    Title = "Leaves and Wonders",
+                    Author = "Laura Phantom",
+                    Description = "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ",
+                    ISBN = "FOT000000001",
+                    ListPrice = 25,
+                    Price = 23,
+                    Price50 = 22,
+                    Price100 = 20,
+                    CategoryId = 2,
+                    ImageUrl = ""
+                }
+                );
+
+                _db.SaveChanges();
+            }
+        }
     }
 }
